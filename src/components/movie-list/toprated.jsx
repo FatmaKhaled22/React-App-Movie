24b073ef90a9ac00d97a336f8f8b1271719d93c8
fatma khaled <fatmakhaled2022999@gmail.com>
2023-10-27 01:23:28 +0200

    replace swiper in home page with other by breakpoints to responsive

--- src/components/movie-list/toprated.jsx
+++ src/components/movie-list/toprated.jsx
@@ -1,38 +1,50 @@
 import React from "react";
-import { Carousel } from '@trendyol-js/react-carousel';
 import { useSelector } from "react-redux";
 import Movie from "./movie";
 import './movies.css';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation} from "swiper/modules";
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
 
 
 function Movies_Top() {
 
-  function RightArrow(){
-    return(<i className="bi bi-arrow-right-circle position-absolute top-50 start-100 translate-middle"></i>);
-  }
-
-  function LeftArrow(){
-    return(<i className="bi bi-arrow-left-circle position-absolute top-50 start-0 translate-middle"></i>);
-  }
-
   const movies = useSelector((state) => state.movies.toprated.results);
   console.log("Movies ---->" , movies);
-
 
   return (
     <>
       <div className="container movies">
-        <div className="row row-cols-1 row-cols-md-3 row-cols-lg-5 g-4 p-3">
-          <Carousel show={5} slide={1} swiping={true} rightArrow={<RightArrow/>} leftArrow={<LeftArrow/>} className="mt-0">
+        <div className="row">
+          <Swiper
+            slidesPerView={1} cssMode={true} navigation={true} spaceBetween={10} modules={[Navigation]}
+            breakpoints={{
+              450: {
+                slidesPerView: 1,
+              },500: {
+                slidesPerView: 2,
+              },768: {
+                slidesPerView: 3,
+              },1000: {
+                slidesPerView: 4,
+              },1400: {
+                slidesPerView: 5,
+              },
+            }}
+          >
             {movies.map((movie)=>{
               return(
-                <div className="m-2">
+                <SwiperSlide key={movie.id} className="p-2">
                   <Movie movie={movie} key={movie.id}/>
-                </div>
+                </SwiperSlide>
               )
             })}
-          </Carousel>
+          </Swiper>
         </div>
+       
       </div>
     </>
   );
--- src/components/trending/trend-tv.jsx
+++ src/components/trending/trend-tv.jsx
@@ -1,37 +1,50 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Carousel } from '@trendyol-js/react-carousel';
 import Tv from "../tv-list/tv";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation} from "swiper/modules";
 import '../movie-list/movies.css';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+
 
 function Trend_Tv() {
-
-  function RightArrow(){
-    return(<i className="bi bi-arrow-right-circle position-absolute top-50 start-100 translate-middle"></i>);
-  }
-
-  function LeftArrow(){
-    return(<i className="bi bi-arrow-left-circle position-absolute top-50 start-0 translate-middle"></i>);
-  }
 
   const tvshows = useSelector((state) => state.trending.trend_tv.results);
   console.log("tv_trend ---->" , tvshows);
 
-
   return (
     <>
-      <div className="container movies mb-5">
-        <div className="row row-cols-1 row-cols-md-3 row-cols-lg-5 g-4 p-3">
-          <Carousel show={5} slide={1} swiping={true} rightArrow={<RightArrow/>} leftArrow={<LeftArrow/>} className="mt-0">
+      <div className="container movies">
+        <div className="row">
+          <Swiper
+            slidesPerView={1} cssMode={true} navigation={true} spaceBetween={10} modules={[Navigation]}
+            breakpoints={{
+              450: {
+                slidesPerView: 1,
+              },500: {
+                slidesPerView: 2,
+              },768: {
+                slidesPerView: 3,
+              },1100: {
+                slidesPerView: 4,
+              },1400: {
+                slidesPerView: 5,
+              },
+            }}
+          >
             {tvshows.map((tvshow)=>{
               return(
-                <div className="m-2" key={tvshow.id}>
+                <SwiperSlide key={tvshow.id} className="p-2">
                   <Tv tv={tvshow} key={tvshow.id}/>
-                </div>
+                </SwiperSlide>
               )
             })}
-          </Carousel>
+          </Swiper>
         </div>
+       
       </div>
     </>
   );
